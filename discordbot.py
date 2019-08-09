826696d1eec9c7ef760c8adc4df7dbcc4e6c0eac
inuinu228 <53936879+inuinu228@users.noreply.github.com>
2019-08-10 01:47:58 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -31,7 +31,7 @@ async def edpi(ctx, a: float, b: float):
     await ctx.send('お前のEDPIは %d や \n わかったか？' % (round(a*b)))
     
 @bot.command()
-async def 180(ctx, a: float, b: float):
+async def furimuki(ctx, a: float, b: float):
     await ctx.send('お前の振り向きは %d cmや \n わかったか？' % ((round(2.54 * 360 / (a * 0.55550 * b)))/2)
 
 bot.run(token)
