b26d6300f5f02ae7dd12010fb6970868af786547
inuinu228 <53936879+inuinu228@users.noreply.github.com>
2019-08-10 02:12:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -20,6 +20,6 @@ async def edpi(ctx, a: float, b: float):
     
 @bot.command()
 async def furimuki(ctx, c: float, d: float):
-    await ctx.send(2.54*180/(c*0.55550*d))
+    await ctx.send(round(2.54*180/(c*0.55550*d)))
      
 bot.run(token)
