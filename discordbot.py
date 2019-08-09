112951d8e5cda0662d60ce99ed2823d09198fec8
inuinu228 <53936879+inuinu228@users.noreply.github.com>
2019-08-09 22:31:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,9 +1,30 @@
-from discord.ext import commands
+from discord.ext 
+import discord
+import commands
 import os
 import traceback
 
+client = discord.Client()
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    
+@client.event
+async def on_message(message):
+    # 「おはよう」で始まるか調べる
+    if message.content.startswith("おはよう"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
+            m = "おはようございます" + message.author.name + "さん！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await message.channel.send(m)
 
 
 @bot.event
@@ -15,4 +36,5 @@ async def on_command_error(ctx, error):
 async def na(ctx):
     await ctx.send('おう')
     
+client.run(token)    
 bot.run(token)
