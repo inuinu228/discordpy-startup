ed96892faec9fa2bfec3d89a767e5042c133c77a
inuinu228 <53936879+inuinu228@users.noreply.github.com>
2019-08-10 01:46:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -29,5 +29,9 @@ async def kakezan(ctx, a: float, b: float):
 @bot.command()
 async def edpi(ctx, a: float, b: float):
     await ctx.send('お前のEDPIは %d や \n わかったか？' % (round(a*b)))
-         
+    
+@bot.command()
+async def 180(ctx, a: float, b: float):
+    await ctx.send('お前の振り向きは %d cmや \n わかったか？' % ((round(2.54 * 360 / (a * 0.55550 * b)))/2)
+
 bot.run(token)
