6161016b9eb32f102589731b68d016bfc185e3c7
inuinu228 <53936879+inuinu228@users.noreply.github.com>
2019-08-10 00:24:47 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -24,7 +24,7 @@ async def oum(ctx, arg):
 
 @bot.command()
 async def kakezan(ctx, a: float, b: float):
-    await ctx.send(a*b)
+    await ctx.send(round(a*b))
     
 @bot.command()
 async def edpi(ctx, a: float, b: float):
