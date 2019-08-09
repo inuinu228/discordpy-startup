11c49fff013350e2ed4b26b442e3578dbc733417
inuinu228 <53936879+inuinu228@users.noreply.github.com>
2019-08-10 02:02:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,21 +15,7 @@ async def na(ctx):
     await ctx.send('おう')
     
 @bot.command()
-async def hey(ctx):
-    await ctx.send('なんや！！！')
-    
-@bot.command()
-async def oum(ctx, arg):
-    await ctx.send(arg)
-
-@bot.command()
-async def kakezan(ctx, a: float, b: float):
-    await ctx.send(round(a*b))
-    
-@bot.command()
 async def edpi(ctx, a: float, b: float):
     await ctx.send('お前のEDPIは %d や \n わかったか？' % (round(a*b)))
     
-
-
 bot.run(token)
