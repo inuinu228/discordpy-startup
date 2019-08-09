6f6f93808013601c50afc93d7da437bb4b560635
inuinu228 <53936879+inuinu228@users.noreply.github.com>
2019-08-09 22:35:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,7 @@
-from discord.ext 
-import discord
-import commands
+from discord.ext import commands
 import os
 import traceback
 
-client = discord.Client()
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
@@ -16,6 +13,5 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def na(ctx):
     await ctx.send('おう')
-    
-client.run(token)    
+
 bot.run(token)
