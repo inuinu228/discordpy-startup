e775613198e1be5ebaaf232cc13582fcbe9e65a7
inuinu228 <53936879+inuinu228@users.noreply.github.com>
2019-08-10 02:02:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,4 +18,8 @@ async def na(ctx):
 async def edpi(ctx, a: float, b: float):
     await ctx.send('お前のEDPIは %d や \n わかったか？' % (round(a*b)))
     
+@bot.command()
+async def zenshu(ctx, a: float, b: float):
+    await ctx.send('お前の全周は %d cmや \n わかったか？' % (2.54 * 360 / (a * 0.55550 * b))
+    
 bot.run(token)
