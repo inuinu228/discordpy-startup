fb54f5959c0603d1fc3b11c07a52ca49f5d9dd14
inuinu228 <53936879+inuinu228@users.noreply.github.com>
2019-08-10 11:50:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,24 +7,6 @@ client = discord.Client()
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-    
- @client.event
-async def on_message(message):
-    # 「やあ」というチャットが来た場合のメッセージ
-    if message.content.startswith("やあ"):
-        # 送り主がチャットボット以外なら返事を返す
-        if client.user != message.author:
-            message = "やあ、" + message.author.name
-            await client.send_message(message.channel, message)
-            
-client.run(token)
-
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
@@ -44,5 +26,6 @@ async def edpi(ctx, a: float, b: float):
 @bot.command()
 async def furimuki(ctx, c: float, d: float):
     await ctx.send('お前の振り向きは %.1f cmや \n わかったか？' % (round(2.54*180/(c*0.55550*d),1)))
-         
+    
+client.run(token)        
 bot.run(token)
