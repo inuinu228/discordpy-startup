50d25414b13da432015dc8d29c1887c8ef5ee6bf
inuinu228 <53936879+inuinu228@users.noreply.github.com>
2019-08-10 11:37:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,6 +4,7 @@ import traceback
 import discord
 
 bot = commands.Bot(command_prefix='/')
+bot = remove_command('help')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 @bot.event
@@ -25,5 +26,12 @@ async def edpi(ctx, a: float, b: float):
 @bot.command()
 async def furimuki(ctx, c: float, d: float):
     await ctx.send('お前の振り向きは %.1f cmや \n わかったか？' % (round(2.54*180/(c*0.55550*d),1)))
+    
+@bot.command()
+async def help(ctx):
+    await ctx.send('/na 俺が同意するぞ \n
+                    /ayamare 俺が謝るぞ \n
+                    /edpi 俺がお前のEDPIを教えるぞ \n
+                    /furimuki 俺がお前の振り向きを教えるぞ'
      
 bot.run(token)
