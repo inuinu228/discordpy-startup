9fe4b1f2368bf7c0d4740e5478135f24d0340d3b
inuinu228 <53936879+inuinu228@users.noreply.github.com>
2019-08-10 18:55:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -42,7 +42,7 @@ async def help(ctx):
 
     embed.add_field(name="/na", value="俺が同意してやるぞ", inline=False)
     embed.add_field(name="/ayamare", value="俺が謝ってやるぞ", inline=False)
-    mbed.add_field(name="/hey", value="俺があいさつ返してやるぞ", inline=False)
+    embed.add_field(name="/hey", value="俺があいさつ返してやるぞ", inline=False)
     embed.add_field(name="/edpi", value="DPIと感度を教えると俺がEDPIを計算してやるぞ", inline=False)
     embed.add_field(name="/furimuki", value="DPIと感度を教えると俺が振り向きを測ってやるぞ", inline=False)
     embed.add_field(name="/help", value="俺がどんなことするか教えてやるぞ", inline=False)
