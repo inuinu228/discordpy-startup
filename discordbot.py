98324c48b02e69a114123e19ee600f47b6606cd1
inuinu228 <53936879+inuinu228@users.noreply.github.com>
2019-08-10 12:09:46 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,16 +3,29 @@ import os
 import traceback
 import discord
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+client = discord.Client()
 
-@client.event
-async def on_message(message):
-    # 「やあ」というチャットが来た場合のメッセージ
-    if message.content.startswith("やあ"):
-        # 送り主がチャットボット以外なら返事を返す
-        if client.user != message.author:
-            message = "やあ、" + message.author.name
-            await client.send_message(message.channel, message)
-
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send(str(error))
+    
+@bot.command()
+async def na(ctx):
+    await ctx.send('おう')
+    
+@bot.command()
+async def ayamare(ctx):
+    await ctx.send('全部俺がわり')
+    
+@bot.command()
+async def edpi(ctx, a: float, b: float):
+    await ctx.send('お前のEDPIは %d や \n わかったか？' % (round(a*b)))
+    
+@bot.command()
+async def furimuki(ctx, c: float, d: float):
+    await ctx.send('お前の振り向きは %.1f cmや \n わかったか？' % (round(2.54*180/(c*0.55550*d),1)))
+       
+bot.run(token)    
 client.run(token) 
