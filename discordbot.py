192d4ce2e2c85e2cea0472b6a62325e0f3711b7d
inuinu228 <53936879+inuinu228@users.noreply.github.com>
2019-08-12 23:48:27 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -47,6 +47,9 @@ async def help(ctx):
     embed.add_field(name="/edpi", value="DPIと感度を教えると俺がEDPIを計算してやるぞ", inline=False)
     embed.add_field(name="/furimuki", value="DPIと感度を教えると俺が振り向きを測ってやるぞ", inline=False)
     embed.add_field(name="/takayama", value="高山の情報を教えるぞ", inline=False)
+    embed.add_field(name="/tokimatsu", value="時松の情報を教えるぞ", inline=False)
+    embed.add_field(name="/ushi", value="牛島の情報を教えるぞ", inline=False)
+    embed.add_field(name="/haruki", value="はるきの情報を教えるぞ", inline=False)
     embed.add_field(name="/help", value="俺がどんなことするか教えてやるぞ", inline=False)
 
     await ctx.send(embed=embed)
@@ -55,17 +58,71 @@ async def help(ctx):
 async def takayama(ctx):
     embed = discord.Embed(title="高山", description="a.k.a 駆け出しチクニー少年", color=0xeee657)
 
-    embed.add_field(name="MOUSE", value="LOGICOOL G502", inline=False)
+    embed.add_field(name="MOUSE", value="Logicool G502h", inline=False)
     embed.add_field(name="DPI", value="1000", inline=False)
     embed.add_field(name="Mouse Sensitivity X", value="0.112", inline=False)
     embed.add_field(name="Mouse Sensitivity Y", value="0.112", inline=False)
     embed.add_field(name="Mouse Targeting Sensitivity", value="0.450", inline=False)
     embed.add_field(name="Mouse Scope Sensitivity", value="0.700", inline=False)
-    embed.add_field(name="Monitor", value="Acer KG251QIbmiipx 24.5", inline=False)
+    embed.add_field(name="Monitor", value="Acer KG251QIbmiipx 24.5 240hz", inline=False)
     embed.add_field(name="GPU", value="GTX 1070", inline=False)
     embed.add_field(name="Mousepad", value="Steelseries Qck Heavy", inline=False)
     embed.add_field(name="Keyboard", value="HyperX Alloy FPS Pro", inline=False)
     embed.add_field(name="Headset", value="Steelseries Arctis5 2017", inline=False)
+    
+    await ctx.send(embed=embed)
+    
+@bot.command()
+async def tokimatsu(ctx):
+    embed = discord.Embed(title="時松", description="a.k.a 駆け出しアナニー少年 more...", color=0xeee657)
+
+    embed.add_field(name="MOUSE", value="Logicool G402", inline=False)
+    embed.add_field(name="DPI", value="1600", inline=False)
+    embed.add_field(name="Mouse Sensitivity X", value="0.06", inline=False)
+    embed.add_field(name="Mouse Sensitivity Y", value="0.06", inline=False)
+    embed.add_field(name="Mouse Targeting Sensitivity", value="0.9", inline=False)
+    embed.add_field(name="Mouse Scope Sensitivity", value="0.9", inline=False)
+    embed.add_field(name="Monitor", value="I-O DATA GigaCrysta 24.5 240hz", inline=False)
+    embed.add_field(name="GPU", value="RTX 2080", inline=False)
+    embed.add_field(name="Mousepad", value="Artisan Shidenkai SnowWhite", inline=False)
+    embed.add_field(name="Keyboard", value="Corsair K63 Red", inline=False)
+    embed.add_field(name="Headset", value="Astro A40TR-MAP&Mix Amp 2019", inline=False)
+    
+    await ctx.send(embed=embed)
+    
+@bot.command()
+async def ushi(ctx):
+    embed = discord.Embed(title="牛島", description="〒880-0122 宮崎県宮崎市塩路1334−3", color=0xeee657)
+
+    embed.add_field(name="MOUSE", value="Logicool G403", inline=False)
+    embed.add_field(name="DPI", value="1600", inline=False)
+    embed.add_field(name="Mouse Sensitivity X", value="0.03", inline=False)
+    embed.add_field(name="Mouse Sensitivity Y", value="0.03", inline=False)
+    embed.add_field(name="Mouse Targeting Sensitivity", value="?", inline=False)
+    embed.add_field(name="Mouse Scope Sensitivity", value="?", inline=False)
+    embed.add_field(name="Monitor", value="BenQ Zowie XL2546 24.5 240hz", inline=False)
+    embed.add_field(name="GPU", value="GTX 1660Ti", inline=False)
+    embed.add_field(name="Mousepad", value="FANTECH M80 mouse pad", inline=False)
+    embed.add_field(name="Keyboard", value="NPET K10", inline=False)
+    embed.add_field(name="Headset", value="SADES Diablo", inline=False)
+    
+    await ctx.send(embed=embed)
+    
+    @bot.command()
+async def haruki(ctx):
+    embed = discord.Embed(title="興梠", description="https://www.vill.nishimera.lg.jp/", color=0xeee657)
+
+    embed.add_field(name="MOUSE", value="Steelseries Rival", inline=False)
+    embed.add_field(name="DPI", value="?", inline=False)
+    embed.add_field(name="Mouse Sensitivity X", value="0.150", inline=False)
+    embed.add_field(name="Mouse Sensitivity Y", value="0.150", inline=False)
+    embed.add_field(name="Mouse Targeting Sensitivity", value="?", inline=False)
+    embed.add_field(name="Mouse Scope Sensitivity", value="?", inline=False)
+    embed.add_field(name="Monitor", value="ViewSonic VX3276-2K-MHD-7", inline=False)
+    embed.add_field(name="GPU", value="GTX 1070", inline=False)
+    embed.add_field(name="Mousepad", value="Learntech PUBG 超大型プロゲームマウスパッド", inline=False)
+    embed.add_field(name="Keyboard", value="Lantoo ゲーミングキーボード 片手", inline=False)
+    embed.add_field(name="Headset", value="?", inline=False)
     
     await ctx.send(embed=embed)
 
