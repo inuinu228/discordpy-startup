aff2fc4e234530065420c5ef8d8e6b4d97fabc27
inuinu228 <53936879+inuinu228@users.noreply.github.com>
2019-08-22 14:35:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -38,11 +38,7 @@ async def furimuki(ctx, c: float, d: float):
 @bot.command()
 async def zoomfurimuki(ctx, c: float, d: float, e: float):
     await ctx.send('お前の振り向きは %.1f cmや \n わかったか？' % (round(2.54*180/(c*0.55550*d*e),1)))
-    
-@bot.command()
-async def Convert(ctx, g: float):
-    await ctx.send('%f cm' % g * math.tan(80 / 2 / 180 * math.PI) / math.tan(106 / 2 / 180 * math.PI)
-    
+        
 bot.remove_command('help')
 
 @bot.command()
